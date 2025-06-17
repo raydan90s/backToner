9bb7b043d97b81a590d1f4e3eea73597edf9c005
Marci <sebhvarg@espol.edu.ec>
2025-06-17 15:39:02 -0500

    arreglo

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
