766170f3c3877ce50c810017954815e5db8c802b
raymich <diegoparrales2005@gmail.com>
2025-06-20 18:38:28 -0500

    poruebas

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'https://tonerexpress-ec.com',
-  'https://www.tonerexpress-ec.com'
+  'https://www.tonerexpress-ec.com',
+  'https://www.novafenix-ec.com'
 ];
 
 // 👇 Preflight (CORS OPTIONS)
