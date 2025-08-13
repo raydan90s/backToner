071c34a8695297065a62e3b76b813af85732c83e
Mar Salvatore <mcvalle@espol.edu.ec>
2025-08-13 14:40:12 -0500

    encriptacion completa

--- shippingAddress.js
+++ shippingAddress.js
@@ -1,38 +1,8 @@
 const pool = require("./db");
 const crypto = require('crypto');
 require('dotenv').config();
+const { cifrar, descifrar } = require('./cifrado');
 
-const secretKey = process.env.SECRET_KEY_ENCRYPTATION;
-
-// Función de cifrado
-const cifrar = (texto) => {
-  if (!texto) return null;
-  
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
-  
-  let cifrado = cipher.update(texto, 'utf8', 'hex');
-  cifrado += cipher.final('hex');
-  
-  return iv.toString('hex') + ':' + cifrado;
-};
-
-// Función de descifrado
-const descifrar = (textoCifrado) => {
-  if (!textoCifrado) return null;
-  
-  const partes = textoCifrado.split(':');
-  const iv = Buffer.from(partes[0], 'hex');
-  const textoCifradoFinal = partes[1];
-  
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
-  let descifrado = decipher.update(textoCifradoFinal, 'hex', 'utf8');
-  descifrado += decipher.final('utf8');
-  
-  return descifrado;
-};
-
-// Función para descifrar un objeto de dirección completo
 const descifrarDireccion = (direccion) => {
   return {
     ...direccion,
